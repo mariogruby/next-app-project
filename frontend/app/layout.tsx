4b5b98996e9e43ec6dcc37b92376896d5fa725aa
mario ruby <mariogabrielrn@gmail.com>
2025-04-06 17:09:00 +0200

    topp laoder

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,10 +1,11 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
-import "./globals.css";
+import { Toaster } from "sonner";
+import NextTopLoader from 'nextjs-toploader';
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 // import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "sonner";
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -21,13 +22,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en"suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body
         className={urbanist.className}>
-          <Navbar />
-          {children}
-          <Toaster />
-          <Footer />
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+          showAtBottom={false}
+        />
+        <Navbar />
+        {children}
+        <Toaster />
+        <Footer />
       </body>
     </html>
   );

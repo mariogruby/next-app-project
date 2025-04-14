21f975c30f1c2f6510daa90f08549721737afb98
mario ruby <mariogabrielrn@gmail.com>
2025-04-14 19:25:10 +0200

    ui changees

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -14,52 +14,62 @@ const Navbar = () => {
     const { lovedItems } = useLovedProducts()
 
     return (
-        <div className="flex items-center justify-between p-4 mx-auto cursor-pointer sm:max-w-4xl md:max-w-6xl">
-            <h1 className="text-3xl" onClick={() => router.push("/")}>none
+        <div className="sticky top-0 z-50 bg-white flex items-center justify-between p-4 mx-auto cursor-pointer sm:max-w-4xl md:max-w-6xl md:rounded-xl">
+            <h1 className="text-3xl" onClick={() => router.push("/")}>E-
                 <span className="font-bold">Store</span>
             </h1>
+
+            {/* menu */}
             <div className="items-center justify-between hidden sm:flex">
                 <MenuList />
             </div>
-            <div className="flex sm:hidden">
+
+            {/* mobile menu  */}
+            <div className="flex items-center gap-3 sm:hidden">
                 <ItemsMenuMobile />
+                <Heart
+                    strokeWidth="1"
+                    className="cursor-pointer"
+                    onClick={() => router.push("/loved-products")}
+                />
+                <ShoppingCart
+                    strokeWidth="1"
+                    className="cursor-pointer"
+                    onClick={() => router.push("/cart")}
+                />
+                <User strokeWidth="1" className="cursor-pointer" />
             </div>
-            <div className="flex items-center justify-between gap-2 sm:gap-7">
-                {cart.items.length == 0 ?
+
+            {/* desktop icons */}
+            <div className="hidden sm:flex items-center justify-between gap-7">
+                {cart.items.length == 0 ? (
                     <ShoppingCart
                         strokeWidth="1"
                         className="cursor-pointer"
-                        onClick={() => router.push("/cart")} />
-                    : (
-                        <div className="flex gap-1" onClick={() => router.push("/cart")}>
-                            <BaggageClaim
-                                strokeWidth={1}
-                                className="cursor-pointer"
-                            />
-                            <span>{cart.items.length}</span>
-                        </div>
-                    )}
-                {lovedItems.length == 0 ?
+                        onClick={() => router.push("/cart")}
+                    />
+                ) : (
+                    <div className="flex gap-1" onClick={() => router.push("/cart")}>
+                        <BaggageClaim strokeWidth={1} className="cursor-pointer" />
+                        <span>{cart.items.length}</span>
+                    </div>
+                )}
+                {lovedItems.length == 0 ? (
                     <Heart
                         strokeWidth="1"
-                        className={"cursor-pointer "}
+                        className="cursor-pointer"
                         onClick={() => router.push("/loved-products")}
                     />
-                    : (
-                        <div className="flex gap-1" onClick={() => router.push("/loved-products")}>
-                            <Heart
-                                strokeWidth={1}
-                                className="cursor-pointer"
-                            />
-                            <span>{lovedItems.length}</span>
-                        </div>
-                    )}
-                < User
-                    strokeWidth="1"
-                    className="cursor-pointer" />
-                {/* <ToggleTheme /> */}
+                ) : (
+                    <div className="flex gap-1" onClick={() => router.push("/loved-products")}>
+                        <Heart strokeWidth={1} className="cursor-pointer" />
+                        <span>{lovedItems.length}</span>
+                    </div>
+                )}
+                <User strokeWidth="1" className="cursor-pointer" />
             </div>
         </div>
+
     );
 };
 
